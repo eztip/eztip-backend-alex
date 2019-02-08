d9c21761ecd9cfdf6784c857d969416f183d7659
Alex <alexander.t.carter@gmail.com>
2019-02-07 18:55:40 -0800

    made usernames unique in db

--- migrations/20190205090636_users.js
+++ migrations/20190205090636_users.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function(users) {
     users.increments();
-    users.string('username', 128).notNullable();
+    users.string('username', 128).notNullable().unique();
     users.string('password', 128).notNullable();
     users.string('email', 128);
     users.string('user_type', 128).notNullable();
